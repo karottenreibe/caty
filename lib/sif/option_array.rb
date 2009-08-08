a5f86c7c38678b989986086b238f669ced6839de
karottenreibe <karottenreibe@gmail.com>
2009-08-08 14:23:19 +0200

    better usage of ohash

--- lib/sif/option_array.rb
+++ lib/sif/option_array.rb
@@ -7,13 +7,11 @@ class Sif::OptionArray < Array
     include Sif::Helpers
 
     def grep!( args )
-        hash = returning(Hash.new) do |hash|
+        returning(OpenHash.new) do |hash|
             self.each do |option|
                 hash[option.name] = option.grep!(args)
             end
         end
-
-        OpenHash.new(hash)
     end
 
 end
