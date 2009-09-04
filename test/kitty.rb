7067732c13026aaed4da9a66130c7a5f03d76c3b
karottenreibe <karottenreibe@gmail.com>
2009-09-04 15:02:40 +0200

    cranked up the kitty program ;-)

--- test/kitty.rb
+++ test/kitty.rb
@@ -2,27 +2,72 @@
 require 'rubygems'
 require 'caty'
 
+#
+# A Kitty.
+#
 class Kitty < Caty
 
     global_options do
-        desc("No meowing")
+
+        #
+        # It's trained not to meow if you tell it to be quiet...
+        #
+        desc('No meowing.')
         boolean :quiet, false
+
     end
 
-    desc("WHAT", "eats something\nWHAT is the food")
-    task_options :s => :string
-    def eat( what )
-        puts "eating #{what} #{task_options.s}!" unless global_options.quiet
+    #
+    # It needs to eat sometimes.
+    #
+    desc('SOMETHING', cut("
+        Eats SOMETHING.
+        You can tell it to -repeat the eating as often as you like.
+    "))
+    task_options :repeat => 1
+
+    def eat( something )
+        task_options.repeat.times { puts "The kitty eats #{something}!" }
+        puts 'meow!' unless global_options.quiet
     end
 
-    desc("sleeps a bit")
-    def sleep
-        puts "sleeping..." unless global_options.quiet
+    #
+    # By default the kitty just meows at you.
+    #
+    def meow
+        puts 'meow!' unless global_options.quiet
     end
 
+    default :meow
+
+    #
+    # If you ask it politely, it will tell you what it can do.
+    #
     help_task
 
 end
 
+#
+# You could put this in a separate file and require it here
+# instead, thus separating any tasks from the others and
+# reducing interdependencies
+#
+Kitty.append do
+
+    #
+    # It can be sleepy...
+    #
+    desc("Sleeps a bit.")
+
+    def sleep
+        puts 'meow!' unless global_options.quiet
+        puts 'The kitty sleeps...'
+    end
+
+end
+
+#
+# Pushes the kitty's on/off switch ;-)
+#
 Kitty.start!
 
